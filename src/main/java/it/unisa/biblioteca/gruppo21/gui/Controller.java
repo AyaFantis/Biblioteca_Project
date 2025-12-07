480ca81e7607967748fc8a61ab7b47b0176bde9f
jdellorfano <j.dellorfano@studenti.unisa.it>
2025-12-07 11:35:34 +0100

    Creazione scheletro classe Controller

--- src/main/java/it/unisa/biblioteca/gruppo21/gui/Controller.java
+++ src/main/java/it/unisa/biblioteca/gruppo21/gui/Controller.java
@@ -5,10 +5,64 @@
  */
 package it.unisa.biblioteca.gruppo21.gui;
 
+import it.unisa.biblioteca.gruppo21.entity.Libro;
+import it.unisa.biblioteca.gruppo21.entity.Prestito;
+import it.unisa.biblioteca.gruppo21.entity.Utente;
+import it.unisa.biblioteca.gruppo21.service.Biblioteca;
+import java.util.List;
+
 /**
  *
  * @author manue
  */
 public class Controller {
     
+    private Biblioteca biblioteca;
+    
+    public Controller(Biblioteca biblioteca) {
+        this.biblioteca = biblioteca;
+    }
+    
+    public void gestisciIscrizione(String nome, String cognome, String matricola, String email) {
+        // TODO
+    }
+    
+    public void gestisciRimozioneUtente(String matricola) {
+        // TODO
+    }
+    
+    public List<Utente> getListaUtenti() {
+        // TODO
+        return null;
+    }
+    
+    public void gestisciAggiuntaLibro(String titolo, String autore, String isbn, String annoStr, String copieStr) {
+        // TODO
+    }
+    
+    public void gestisciRimozioneLibro(String isbn) {
+        // TODO
+    }
+    
+    public List<Libro> getListaLibri() {
+        // TODO
+        return null;
+    }
+   
+    public void gestisciPrestito(String matricola, String isbn) {
+        // TODO
+    }
+    
+    public void gestisciRestituzione(String matricola, String isbn) {
+        // TODO
+    }
+    
+    public List<Prestito> getListaPrestiti() {
+        // TODO
+        return null;
+    }
+ 
+    private void mostraMessaggio(String messaggio) {
+        // TODO
+    }
 }
