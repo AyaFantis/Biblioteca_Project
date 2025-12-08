c2b1cc9e7ac4794dd5be27627aaed762379dcb6a
SimoLucky04 <simonederiggi8@gmail.com>
2025-12-08 17:10:40 +0100

    documentazione per Doxyfile App

--- src/main/java/it/unisa/biblioteca/gruppo21/App.java
+++ src/main/java/it/unisa/biblioteca/gruppo21/App.java
@@ -10,15 +10,35 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
+/**
+ * @file App.java
+ * @brief Punto di ingresso (Entry Point) dell'applicazione JavaFX.
+ * @class App
+ * Il suo compito è istanziare i componenti principali dell'architettura MVC/MVP
+ * (Model, View, Controller), collegarli tra loro, e avviare 
+ * il ciclo di vita dell'interfaccia grafica.
+ * @author Gruppo 21
+ * @version 1.0
+ */
 public class App extends Application {
+    
+    /**
+     * @brief Metodo di avvio del ciclo di vita JavaFX.
+     * Viene chiamato automaticamente dal runtime JavaFX dopo il metodo main.
+     * Esegue la configurazione iniziale, mostrandola sullo Stage primario.
+     * @param primaryStage la finestra principale fornita da JavaFX.
+     */
  @Override
     public void start(Stage primaryStage) {
     }
     
     /**
-     * @param args the command line arguments
+     * @brief Metodo Main standard di Java.
+     * Punto di ingresso assoluto del programma.
+     * Delegare l'esecuzione al launcher di JavaFX chiamando launch()}.
+     * @param args Argomenti da riga di comando.
      */
     public static void main(String[] args) {
+        launch(args);
     }
 }
