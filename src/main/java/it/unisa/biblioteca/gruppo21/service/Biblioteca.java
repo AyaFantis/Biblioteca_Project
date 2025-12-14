00a25995bc08593fa00e839795ccf81fed6085e9
jdellorfano <j.dellorfano@studenti.unisa.it>
2025-12-15 00:04:58 +0100

    Doxygen Biblioteca

--- src/main/java/it/unisa/biblioteca/gruppo21/service/Biblioteca.java
+++ src/main/java/it/unisa/biblioteca/gruppo21/service/Biblioteca.java
@@ -18,23 +18,26 @@ import java.util.List;
 /**
  * @file Biblioteca.java
  * @brief Classe Facade che espone le funzionalità principali del sistema.
- * @class Biblioteca
+ ** @class Biblioteca
  * Questa classe agisce come un "pannello di controllo" unificato
  * nascondendo la complessità dei sottosistemi (Archivi e Servizi) al livello superiore (Controller/ViewLibreria).
  * Gestisce il ciclo di vita (creazione e iniezione delle dipendenze) dei componenti di backend.
- * @author Gruppo 21
+ ** @author Gruppo 21
  * @version 1.0
  */
 public class Biblioteca {
-   
+    /** Riferimento al service per la gestione degli utenti */
     private final ServiceUtenti serviceUtenti;
+    /** Riferimento al service per la gestione dei libri */
     private final ServiceLibri serviceLibri;
+    /** Riferimento al service per la gestione dei prestiti */
     private final ServicePrestiti servicePrestiti;
 
     /**
      * @brief Costruttore della classe Biblioteca.
-     * Inizializza gli archivi, collegando le dipendenze
-     * @post Il sistema è pronto per elaborare richieste.
+     ** Inizializza gli archivi di Utenti, Libri e Prestiti, collegando le dipendenze necessarie.
+     * Durante l'inizializzazione, ripristina i collegamenti tra prestiti attivi e utenti in memoria.
+     ** @post Il sistema è pronto per elaborare richieste.
      */
     public Biblioteca(){
     
@@ -58,17 +61,18 @@ public class Biblioteca {
     }
     
     //
-    // METODI UTENTE
+    // METODI SERVICE UTENTE
     //
     
     /**
      * @brief Registra un nuovo utente nel sistema.
-     * Passa l'operazione al ServiceUtenti.
-     * @param nome Nome dell'utente.
+     ** Passa l'operazione al ServiceUtenti.
+     * Vengono effettiati controlli sulla validità della matricola e dell'email.
+     ** @param nome Nome dell'utente.
      * @param cognome Cognome dell'utente.
      * @param matricola Matricola univoca (10 cifre).
      * @param email Email istituzionale (@studenti.unisa.it).
-     * @return Messaggio di esito.
+     * @return Una stringa contenete un messaggio di successo o la descrizione dell'errore
      */
     public String iscriviUtente(String nome, String cognome, String matricola, String email){
         return serviceUtenti.iscrivi(nome, cognome, matricola, email);
@@ -76,14 +80,14 @@ public class Biblioteca {
     
     /**
      * @brief Modifica i dati anagrafici di un utente esistente.
-     * Permette di aggiornare Nome, Cognome ed Email. La Matricola viene usata come chiave di ricerca
+     ** Permette di aggiornare Nome, Cognome ed Email. La Matricola viene usata come chiave di ricerca
      * e non può essere modificata.
      * Passa l'operazione al ServiceUtenti.
-     * @param nuovoNome Il nuovo nome da impostare.
+     ** @param nuovoNome Il nuovo nome da impostare.
      * @param nuovoCognome Il nuovo cognome da impostare.
      * @param nuovaEmail La nuova email da impostare.
      * @param matricola La matricola dell'utente da modificare.
-     * @return Messaggio di esito.
+     * @return Una stringa contenete l'esito dell'operazione
      */
     public String modificaUtente(String nuovoNome, String nuovoCognome, String nuovaEmail, String matricola){
         return serviceUtenti.modificaDatiUtente(nuovoNome, nuovoCognome, nuovaEmail, matricola);
@@ -91,10 +95,10 @@ public class Biblioteca {
 
     /**
      * @brief Rimuove un utente dal sistema.
-     * L'operazione viene rifiutata se l'utente ha ancora prestiti attivi.
+     ** L'operazione viene rifiutata se l'utente ha ancora prestiti attivi.
      * Passa l'operazione al ServiceUtenti.
-     * @param matricola La matricola dell'utente da rimuovere.
-     * @return Messaggio che descrive l'esito dell'operazione.
+     ** @param matricola La matricola dell'utente da rimuovere.
+     * @return Una stringa che descrive l'esito dell'operazione.
      */
     public String rimuoviUtente(String matricola){
         return serviceUtenti.rimuovi(matricola);
@@ -102,7 +106,7 @@ public class Biblioteca {
     
     /**
      * @brief Recupera l'elenco completo degli utenti registrati.
-     * Passa l'operazione al ServiceUtenti.
+     ** Passa l'operazione al ServiceUtenti.
      * @return Lista di oggetti Utente.
      */
     public List<Utente> getElencoUtenti() { 
@@ -111,7 +115,7 @@ public class Biblioteca {
     
     /**
      * @brief Cerca utenti in base a criteri anagrafici.
-     * Passa l'operazione a ServiceUtenti.
+     ** Passa l'operazione a ServiceUtenti.
      * @param cognome Filtro sul cognome (opzionale).
      * @param matricola Filtro sulla matricola (opzionale).
      * @return Lista degli utenti che corrispondono ai criteri specificati.
@@ -121,18 +125,18 @@ public class Biblioteca {
     }
  
     // 
-    //   METODI LIBRO
+    //   METODI SERVICE LIBRO
     //
     
     /**
      * @brief Aggiunge un nuovo libro al catalogo.
-     * Passa l'operazione a ServiceLibri.
+     ** Passa l'operazione a ServiceLibri.
      * @param titolo Titolo del libro.
      * @param autore Autore/i del libro.
      * @param codiceISBN Codice ISBN univoco.
      * @param annoPubblicazione Anno di pubblicazione.
      * @param numeroCopieDisponibili Numero totale di copie fisiche.
-     * @return Messaggio che descrive l'esito dell'operazione.
+     * @return Una stringa che descrive l'esito dell'operazione.
      */
     public String aggiungiLibro(String titolo, String autore, String codiceISBN, int annoPubblicazione, int numeroCopieDisponibili){
         return serviceLibri.aggiungi(titolo, autore, codiceISBN, annoPubblicazione, numeroCopieDisponibili);
@@ -140,9 +144,9 @@ public class Biblioteca {
     
     /**
      * @brief Aggiorna il numero di copie disponibili di un libro.
-     * @param isbn Codice ISBN del libro.
+     ** @param isbn Codice ISBN del libro.
      * @param nuoveCopie Nuovo numero totale di copie.
-     * @return Messaggio di esito.
+     * @return Una stringa contenente
      */
     public String aggiornaCopieLibro(String isbn, int nuoveCopie) {
         return serviceLibri.aggiornaCopie(isbn, nuoveCopie);
@@ -150,10 +154,10 @@ public class Biblioteca {
 
     /**
      * @brief Rimuove un libro dal catalogo.
-     * Operazione consentita solo se non ci sono copie in prestito.
+     ** Operazione consentita solo se non ci sono copie in prestito.
      * Passa l'operazione a ServiceLibri.
-     * @param codiceISBN Il codice ISBN del libro da rimuovere.
-     * @return Messaggio di esito.
+     ** @param codiceISBN Il codice ISBN del libro da rimuovere.
+     * @return Una stringa contenete l'esito dell'operazione.
      */
     public String rimuoviLibro(String codiceISBN){
        return serviceLibri.rimuovi(codiceISBN); 
@@ -161,7 +165,7 @@ public class Biblioteca {
     
     /**
      * @brief Recupera l'elenco completo dei libri nel catalogo.
-     * Passa l'operazione a ServiceLibri.
+     ** Passa l'operazione a ServiceLibri.
      * @return Lista di oggetti Libro.
      */
     public List<Libro> getElencoLibri() { 
@@ -170,8 +174,8 @@ public class Biblioteca {
     
     /**
      * @brief Cerca libri per titolo, autore o ISBN.
-     * Passa l'operazione a ServiceLibri.
-     * @param titolo Titolo da cercare.
+     ** Passa l'operazione a ServiceLibri.
+     ** @param titolo Titolo da cercare.
      * @param autore Autore da cercare.
      * @param codiceISBN ISBN da cercare.
      * @return Lista dei libri trovati.
@@ -181,17 +185,17 @@ public class Biblioteca {
     }
     
     //
-    //METODI PRESTITO
+    //METODI SERVICE PRESTITO
     //
 
     /**
      * @brief Registra un nuovo prestito per un utente.
-     * Verifica la disponibilità del libro e il limite massimo di prestiti per utente.
+     ** Verifica la disponibilità del libro e il limite massimo di prestiti per utente.
      * Passa l'operazione a ServicePrestiti.
-     * @param identificativo Nome o cognome dell'utente richiedente.
+     ** @param identificativo Nome o cognome dell'utente richiedente.
      * @param codiceISBN L'ISBN del libro richiesto.
      * @param dataScadenza Data di restituzione.
-     * @return Messaggio di esito.
+     * @return Una stringa contenete l'esito (Successo o Errore).
      */
     public String effettuaPrestito(String identificativo, String codiceISBN, LocalDate dataScadenza){
         return servicePrestiti.nuovoPrestito(identificativo, codiceISBN, dataScadenza);
@@ -199,11 +203,11 @@ public class Biblioteca {
     
     /**
      * @brief Registra la restituzione di un libro.
-     * Libera una copia del libro.
+     ** Aggiorna lo stato del prestito a RESTITUITO e incrementa le copie disponibili
      * Passa l'operazione a ServicePrestiti
-     * @param matricola La matricola dell'utente che restituisce.
+     ** @param matricola La matricola dell'utente che restituisce.
      * @param codiceISBN L'ISBN del libro restituito.
-     * @return Messaggio di esito.
+     * @return Una stringa contenete l'esito dell'operazione
      */
     public String restituisciLibro(String matricola, String codiceISBN){
         return servicePrestiti.restituzione(matricola, codiceISBN);
@@ -211,9 +215,9 @@ public class Biblioteca {
     
     /**
      * @brief Recupera lo storico completo di tutti i prestiti registrati.
-     * Include sia i prestiti attivi che quelli conclusi.
+     * *Include sia i prestiti attivi che quelli conclusi (restituiti).
      * Passa l'operazione a ServicePrestiti.
-     * @return Lista di oggetti Prestito.
+     * *@return Lista di oggetti Prestito.
      */
     public List<Prestito> getStoricoPrestiti(){
         return servicePrestiti.getLista();
@@ -223,7 +227,6 @@ public class Biblioteca {
      * @brief Controlla la presenza di ritardi nei prestiti attivi.
      * Scansiona i prestiti e aggiorna lo stato a IN_RITARDO se la data di scadenza è superata.
      * Passa l'operazione a ServicePrestiti.
-     * Può innescare l'invio di notifiche.
      */
     public void controllaRitardi(){
        servicePrestiti.controllaRitardi();
